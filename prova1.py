6427fff46a36c058ceb688fba439b4d13b2345a3
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2023-11-19 15:14:08 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,17 @@
+# Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+string = "nome qualquer"    #str
+inteiro = 12                #int
+decimal = 50.23             #Float
+logico = True               #bool
+
+print(type(string))
+print(type(inteiro))
+print(type(decimal))
+print(type(logico))
+
+# Crie uma solicitação de dados ao usuário. Imprima a solicitação anterior na tela com uma mensagem personalizada.
+
+nome= input("Digite seu nome: ")
+
+print(f" Olá {nome}\n Seja bem vindo!")
